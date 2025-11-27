5d6d9765df22dfef156aaefcb76a08a7ed3c4a4d
insanetim <47120724+insanetim@users.noreply.github.com>
2025-11-27 09:37:57 +0200

    add rtk

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
 import { Route, BrowserRouter as Router, Routes } from "react-router"
 import App from "./App.tsx"
 import "./index.css"
+import { store } from "./store"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route
-          path="/*"
-          element={<App />}
-        />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route
+            path="/*"
+            element={<App />}
+          />
+        </Routes>
+      </Router>
+    </Provider>
   </StrictMode>
 )
